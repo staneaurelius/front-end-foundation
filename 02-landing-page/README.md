0758b46158134cc240db9020b760dcf615ffe7ba
staneaurelius <staneaurelius@gmail.com>
2024-07-25 19:58:48 +0700

    Restructure folder - Add project number for order

---
+++ 02-landing-page/README.md
@@ -0,0 +1,12 @@
+# Project: Landing Page
+
+This project is part of **The Odin Project's** Foundational course. This project is meant for demonstrating the proficiency to create an entire web page from a design using both HTML and CSS. This project requires an understanding of both *HTML structure* and *CSS styling* concept. The CSS styling concepts used in this project is basic CSS and *flexbox* (for layout)
+
+## Project Result
+
+![A preview of the project result](static/project-result.png)
+
+## License & Attribution
+
+- The Bitcoin image in the *Home* section is designed by [Freepik](https://www.freepik.com/free-vector/cryptocurrency-bitcoin-golden-coin-background_13891828.htm#query=bitcoin&position=0&from_view=keyword&track=sph&uuid=698d1761-4991-468f-ac8b-f11b39b10f61)
+- The illustration provided in the *Products & Features* section is designed by [unDraw](https://undraw.co/)
---
+++ 03-rock-paper-scissors/script.js
@@ -0,0 +1,112 @@
+// Check if script is connected
+console.log('Welcome to Rock-Paper-Scissors: the Game!');
+
+
+// Randomize computer choice
+function getComputerChoice() {
+    const randomNumber = Math.random();
+    let computerChoice;
+
+    if (randomNumber < 1/3) {
+        computerChoice = 'rock';
+    } else if (randomNumber > 2/3) {
+        computerChoice = 'paper';
+    } else {
+        computerChoice = 'scissors';
+    };
+
+    return computerChoice;
+};
+
+
+// Prompt user to input hand signal
+function getHumanChoice() {
+    let humanChoice;
+    let validity = false;
+
+    while (!validity) {
+        humanChoice = prompt('What is your hand signal? (Rock / Paper / Scissors)').toLowerCase();
+        switch (humanChoice) {
+            case 'rock':
+                validity = true;
+                break;
+            case 'scissors':
+                validity = true;
+                break;
+            case 'paper':
+                validity = true;
+                break;
+            default:
+                console.log('Please enter a valid hand signal!');
+        };
+    };
+
+    return humanChoice;
+};
+
+
+// Playing a round
+function playRound(humanChoice, computerChoice) {
+    let winner;
+
+    if (humanChoice === computerChoice) {
+        winner = null;
+        console.log(`Computer also choose ${computerChoice}, it's a draw!`);
+    } else {
+        switch (humanChoice) {
+            case 'rock':
+                if (computerChoice === 'paper') {
+                    winner = 'computer';
+                    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+                } else {
+                    winner = 'human';
+                    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+                };
+                break;
+
+            case 'paper':
+                if (computerChoice === 'scissors') {
+                    winner = 'computer';
+                    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+                } else {
+                    winner = 'human';
+                    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+                };
+                break;
+
+            case 'scissors':
+                if (computerChoice === 'rock') {
+                    winner = 'computer';
+                    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+                } else {
+                    winner = 'human';
+                    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+                };
+                break;
+        };
+    };
+
+    return winner;
+};
+
+
+// Final game
+function playGame() {
+    let humanScore = 0,
+        computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        const winner = playRound(humanSelection, computerSelection);
+        
+        // Game scoring
+        if (winner) {
+            winner === 'human' ? humanScore++ : computerScore++;
+        };
+
+        console.log(`Current score:\n    Player: ${humanScore}\n    Computer: ${computerScore}`);
+    };
+};
+
+playGame();
