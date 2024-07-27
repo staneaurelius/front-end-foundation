f7c11ebaab7a12cee08d1235095f05fe074e9209
staneaurelius <staneaurelius@gmail.com>
2024-07-27 20:05:54 +0800

    Add hovering functionality - Add logic to change cell color on mouse hover

--- 04-etch-a-sketch/js/script.js
+++ 04-etch-a-sketch/js/script.js
@@ -1,15 +1,24 @@
 console.log('Ethc-a-Sketch!');
 
+// Generate Square divs
 const mainContainer = document.querySelector('.container');
-
 for (let i=0; i<16; i++) {
     const rowContainer = document.createElement('div');
     rowContainer.classList.add('row-container');
 
     for (let j=0; j<16; j++) {
         const grid = document.createElement('div');
+        grid.classList.add('grid');
         rowContainer.appendChild(grid);
     };
 
     mainContainer.appendChild(rowContainer);
 };
+
+// Add onMouseOver event listener
+const squareGrids = document.querySelectorAll('.grid');
+squareGrids.forEach((squareGrid) => {
+    squareGrid.addEventListener('mouseover', (e) => {
+        e.target.classList.add('active');
+    });
+});
