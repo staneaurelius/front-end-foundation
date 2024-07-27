0b9d9aaa1b652848c57b400216ef0219f1db85c5
staneaurelius <staneaurelius@gmail.com>
2024-07-27 21:56:20 +0800

    Add grid size change and clear drawing functionality

--- 04-etch-a-sketch/js/script.js
+++ 04-etch-a-sketch/js/script.js
@@ -1,24 +1,57 @@
 console.log('Ethc-a-Sketch!');
 
-// Generate Square divs
-const mainContainer = document.querySelector('.container');
-for (let i=0; i<16; i++) {
-    const rowContainer = document.createElement('div');
-    rowContainer.classList.add('row-container');
+// Parse & show slider input value
+let gridSize = 16;
+const sizeSlider = document.querySelector('input');
+const sizeText = document.querySelector('span');
 
-    for (let j=0; j<16; j++) {
-        const grid = document.createElement('div');
-        grid.classList.add('grid');
-        rowContainer.appendChild(grid);
+sizeSlider.addEventListener('input', (e) => {
+    gridSize = sizeSlider.value;
+    sizeText.textContent = gridSize;
+});
+
+// Function for generating new square divs
+function generateGrid (mainContainer, gridSize) {
+    let cellSize = 480/gridSize;
+
+    while (mainContainer.firstChild) {
+        mainContainer.removeChild(mainContainer.lastChild);
     };
 
-    mainContainer.appendChild(rowContainer);
+    for (let i=0; i<gridSize; i++) {
+        const rowContainer = document.createElement('div');
+        rowContainer.classList.add('row-container');
+        rowContainer.style.cssText = `width: 480px; height: ${cellSize}px`
+
+        for (let j=0; j<gridSize; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.style.cssText = `width: ${cellSize}px; height: ${cellSize}px`
+            rowContainer.appendChild(cell);
+        };
+
+        mainContainer.appendChild(rowContainer);
+    };
 };
 
-// Add onMouseOver event listener
-const squareGrids = document.querySelectorAll('.grid');
-squareGrids.forEach((squareGrid) => {
-    squareGrid.addEventListener('mouseover', (e) => {
-        e.target.classList.add('active');
+// Enable cell coloring
+function colorCells () {
+    const squareCells = document.querySelectorAll('.cell');
+    squareCells.forEach((squareCell) => {
+        squareCell.addEventListener('mouseover', (e) => {
+            e.target.classList.add('active');
+        });
     });
+};
+
+// Generate colorable cells
+const mainContainer = document.querySelector('.grid-container');
+generateGrid(mainContainer, gridSize);
+colorCells();
+
+// Event listener for changing grid size
+const clearButton = document.querySelector('button');
+clearButton.addEventListener('click', (e) => {
+    generateGrid(mainContainer, gridSize);
+    colorCells();
 });
