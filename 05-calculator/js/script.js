f90bc426e2228882f3d264e9d463d66383214430
staneaurelius <staneaurelius@gmail.com>
2024-07-28 21:55:23 +0800

    Calculator - Add basic math operation

--- 05-calculator/js/script.js
+++ 05-calculator/js/script.js
@@ -1,1 +1,18 @@
 console.log('Welcome to Calculate!');
+
+// Basic math operations
+function add (numberOne, numberTwo) {
+    return numberOne + numberTwo;
+};
+
+function subtract (numberOne, numberTwo) {
+    return numberOne - numberTwo;
+};
+
+function multiply (numberOne, numberTwo) {
+    return numberOne * numberTwo;
+};
+
+function divide (numberOne, numberTwo) {
+    return numberOne / numberTwo;
+};
