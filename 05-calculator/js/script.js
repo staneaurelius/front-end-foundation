d4adac6652a1de61d8ab76270889db6d33588c5e
staneaurelius <staneaurelius@gmail.com>
2024-07-28 22:01:47 +0800

    Calculator - add operate function - Add operate function for evaluating calculation - Initiate 3 variables for calculator display

--- 05-calculator/js/script.js
+++ 05-calculator/js/script.js
@@ -16,3 +16,19 @@ function multiply (numberOne, numberTwo) {
 function divide (numberOne, numberTwo) {
     return numberOne / numberTwo;
 };
+
+function operate (numberOne, operator, numberTwo) {
+    switch (operator) {
+        case "+":
+            return add(numberOne, numberTwo);
+        case "-":
+            return subtract(numberOne, numberTwo);
+        case "x":
+            return multiply(numberOne, numberTwo);
+        case "/":
+            return divide(numberOne, numberTwo);
+    };
+};
+
+// Calculator display
+let firstNumber, operator, secondNumber;
