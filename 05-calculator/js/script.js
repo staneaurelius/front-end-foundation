6b72c09f9735e088d9d5ac223c685216e939f4c4
staneaurelius <staneaurelius@gmail.com>
2024-07-30 14:11:03 +0800

    Reformat calculator project code - Rewrite code to be more concise and readable - Add Error when performing division by 0 - Limit decimal to 1 comma

--- 05-calculator/js/script.js
+++ 05-calculator/js/script.js
@@ -26,129 +26,148 @@ function operate (numberOne, operator, numberTwo) {
         case "×":
             return multiply(numberOne, numberTwo);
         case "÷":
-            return divide(numberOne, numberTwo);
+            if (numberTwo == 0) {
+                return 'Error!';
+            } else {
+                return divide(numberOne, numberTwo);
+            };
     };
 };
 
-// Calculator display
+// Calculator logic
 let firstNumber,
     operator,
     secondNumber,
     afterEqual;
+const displayedNumber = document.querySelector(".result");
+const equation = document.querySelector('.equation');
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
+// Number buttons behavior
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        const buttonType = e.target.classList[0];
-        const displayedNumber = document.querySelector(".result");
-        const equation = document.querySelector('.equation');
+        // If number if pressed, limit to 10 number and remove all active operator
+        if (document.querySelector('.active')) {
+            const activeOperator = document.querySelector('.active');
+            activeOperator.classList.remove('active');
+            displayedNumber.textContent = "0";
+        };
 
-        switch (buttonType) {
-            case 'number':
-                // If number if pressed, limit to 10 number and remove all active operator
+        if (displayedNumber.textContent.length < 10) {
+            const buttonValue = e.target.value;
+
+            // Parse number if not decimal
+            if (buttonValue === 'decimal' && !displayedNumber.textContent.includes('.')) {
+                displayedNumber.textContent = displayedNumber.textContent.concat('.')
+            } else if (buttonValue != 'decimal') {
+                parsedNumber = displayedNumber.textContent.concat(buttonValue);
+                displayedNumber.textContent = Number(parsedNumber);
+            };
+        };
+    });
+});
+
+// Operator buttons behavior
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        // If operator is pressed, store the current number in firstNumber and get operator value
+        if (!document.querySelector('.active')) {
+            e.target.classList.add('active');
+            
+            if (firstNumber && !afterEqual) {
+                // If there is preceeding number, directly commence operation
+                secondNumber = Number(displayedNumber.textContent);
+                equation.textContent = `${firstNumber} ${operator} ${secondNumber}`
+                operationResult = operate(firstNumber, operator, secondNumber);
+
+                displayedNumber.textContent = operationResult;
+                firstNumber = operationResult;
+            } else {
+                // Else store the current number & operator
+                firstNumber = Number(displayedNumber.textContent);
+            };
+            operator = e.target.value;
+            afterEqual = false;
+        };
+    });
+});
+
+// Single operator buttons
+const singleButtons = document.querySelectorAll('.single');
+singleButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        const buttonValue = e.target.value;
+
+        switch (buttonValue) {
+            case 'reset':
+                // On AC reset all to default state
                 if (document.querySelector('.active')) {
-                    const activeOperator = document.querySelector('.active');
-                    activeOperator.classList.remove('active');
-                    displayedNumber.textContent = "0";
+                    e.target.classList.remove('active');
                 };
+                firstNumber = null;
+                operator = null;
+                secondNumber = null;
+                afterEqual = null;
+                equation.textContent = '';
+                displayedNumber.textContent = '0';
+                break
 
-                if (displayedNumber.textContent.length < 10) {
-                    const buttonValue = e.target.value;
-                    parsedNumber = displayedNumber.textContent.concat(buttonValue);
-                    parsedNumber = Number(parsedNumber).toString();
-                    displayedNumber.textContent = parsedNumber;
+            case 'equal':
+                if (secondNumber && afterEqual) {
+                    // Pressing "=" after operation redo the operation
+                    equation.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+                    operationResult = operate(firstNumber, operator, secondNumber);
+
+                    displayedNumber.textContent = operationResult;
+                    firstNumber = operationResult;
+                } else if (firstNumber && operator) {
+                    // If there's no second number, use own self for operation
+                    secondNumber = Number(displayedNumber.textContent);
+                    equation.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+                    operationResult = operate(firstNumber, operator, secondNumber);
+
+                    displayedNumber.textContent = operationResult;
+                    firstNumber = operationResult;
                 };
+                afterEqual = true;
                 break;
             
-            case 'operator':
-                // If operator is pressed, store the current number in firstNumber and get operator value
-                if (!document.querySelector('.active')) {
-                    e.target.classList.add('active');
-                    
-                    if (firstNumber && !afterEqual) {
-                        // If there is preceeding number, commence operation
-                        secondNumber = Number(displayedNumber.textContent);
-                        equation.textContent = `${firstNumber} ${operator} ${secondNumber}`
-                        operationResult = operate(firstNumber, operator, secondNumber);
-                        displayedNumber.textContent = operationResult;
-                        firstNumber = operationResult;
-                    } else {
-                        // Else store the current number & operator
-                        firstNumber = Number(displayedNumber.textContent);
-                    };
-                    operator = e.target.value;
-                    afterEqual = false;
-                };
+            case 'percent':
+                // On %, set secondNumber to be 100 and perform operation
+                firstNumber = Number(displayedNumber.textContent);
+                secondNumber = 100;
+                equation.textContent = `${firstNumber}%`
+                operationResult = operate(firstNumber, '÷', secondNumber);
+
+                displayedNumber.textContent = operationResult;
+                firstNumber = operationResult;
+                afterEqual = true;
                 break;
 
-            case 'single':
-                // Logic for single number operation
-                const buttonValue = e.target.value;
-                switch (buttonValue) {
-                    case 'reset':
-                        if (document.querySelector('.active')) {
-                            e.target.classList.remove('active');
-                        };
-                        firstNumber = null;
-                        operator = null;
-                        secondNumber = null;
-                        afterEqual = null;
-                        equation.textContent = '';
-                        displayedNumber.textContent = '0';
-                        break
-
-                    case 'equal':
-                        if (secondNumber && afterEqual) {
-                            // Pressing "=" after operation redo the operation
-                            equation.textContent = `${firstNumber} ${operator} ${secondNumber}`
-                            operationResult = operate(firstNumber, operator, secondNumber);
-                            displayedNumber.textContent = operationResult;
-                            firstNumber = operationResult;
-                        } else if (firstNumber && operator) {
-                            // If there's no second number, use own self for operation
-                            secondNumber = Number(displayedNumber.textContent);
-                            equation.textContent = `${firstNumber} ${operator} ${secondNumber}`
-                            operationResult = operate(firstNumber, operator, secondNumber);
-                            displayedNumber.textContent = operationResult;
-                            firstNumber = operationResult;
-                        };
-                        afterEqual = true;
-                        break;
-                    
-                    case 'percent':
-                        firstNumber = Number(displayedNumber.textContent);
-                        secondNumber = 100;
-                        equation.textContent = `${firstNumber}%`
-                        operationResult = operate(firstNumber, '÷', secondNumber);
-                        displayedNumber.textContent = operationResult;
-                        firstNumber = operationResult;
-                        afterEqual = true;
-                        break;
-
-                    case 'negate':
-                        if (firstNumber == displayedNumber.textContent) {
-                            firstNumber = Number(-firstNumber);
-                        };
-                        displayedNumber.textContent = Number(-displayedNumber.textContent);
-                        break;
-
-                    case 'delete':
-                        let correctedText;
-                        if (displayedNumber.textContent.length > 1) {
-                            correctedText = displayedNumber.textContent.slice(0, -1);
-                        } else {
-                            correctedText = "0";
-                        };
-                        if (firstNumber == displayedNumber.textContent) {
-                            firstNumber = correctedText;
-                        };
-                        displayedNumber.textContent = correctedText;
-                        break;
-
-                    case 'decimal':
-                        displayedNumber.textContent = displayedNumber.textContent.concat('.');
-                        break
+            case 'negate':
+                // On negate, inverse the number and change equation
+                if (firstNumber == displayedNumber.textContent) {
+                    firstNumber = Number(-firstNumber);
                 };
+                displayedNumber.textContent = Number(-displayedNumber.textContent);
+                break;
+
+            case 'delete':
+                let correctedText;
+                // Set to 0 if there's only 1 number
+                if (displayedNumber.textContent.length > 1) {
+                    correctedText = displayedNumber.textContent.slice(0, -1);
+                } else {
+                    correctedText = "0";
+                };
+
+                // Change the stored number in the equation
+                if (firstNumber == displayedNumber.textContent) {
+                    firstNumber = correctedText;
+                };
+                displayedNumber.textContent = correctedText;
+                break;
         };
     });
 });
